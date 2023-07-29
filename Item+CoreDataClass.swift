6f50912e55a6589f23876eb8fbd6fbfa58a2a22d
Tacettin Kustu <tacettinkustu@gmail.com>
2023-07-29 14:32:53 +0300

    Create NSManagedObject class

---
+++ Item+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Item+CoreDataClass.swift
+//  Devote
+//
+//  Created by Tacettin Küstü on 29.07.2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Item)
+public class Item: NSManagedObject {
+
+}
