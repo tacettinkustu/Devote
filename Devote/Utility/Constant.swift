a5e0f46bed7907d0e1dc50fe6dd27ac35d0fbc54
Tacettin Kustu <tacettinkustu@gmail.com>
2023-07-31 16:45:11 +0300

    Move itemFormatter function

---
+++ Devote/Utility/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Tacettin Küstü on 31.07.2023.
+//
+
+import SwiftUI
+
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
