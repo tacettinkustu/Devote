341a5aec61427a5b4a13e079e6a643b62e759261
Tacettin Kustu <tacettinkustu@gmail.com>
2023-08-06 18:46:26 +0300

    Create a new backgroundImageView component

---
+++ Devote/View/BackgroundImageView.swift
@@ -0,0 +1,24 @@
+//
+//  BackgroundImageView.swift
+//  Devote
+//
+//  Created by Tacettin Küstü on 6.08.2023.
+//
+
+import SwiftUI
+
+struct BackgroundImageView: View {
+    var body: some View {
+        Image("rocket")
+            .antialiased(true)
+            .resizable()
+            .scaledToFill()
+            .ignoresSafeArea(.all)
+    }
+}
+
+struct BackgroundImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundImageView()
+    }
+}
