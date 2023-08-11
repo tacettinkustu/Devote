2c6b74322846c925d7a7fa443ae0c85b92954059
Tacettin Kustu <tacettinkustu@gmail.com>
2023-08-11 22:01:30 +0300

    Create a new ListRowItemView component

---
+++ Devote/View/ListRowItemView.swift
@@ -0,0 +1,29 @@
+//
+//  ListRowItemView.swift
+//  Devote
+//
+//  Created by Tacettin Küstü on 11.08.2023.
+//
+
+import SwiftUI
+
+struct ListRowItemView: View {
+    @Environment(\.managedObjectContext) var viewContext
+    @ObservedObject var item: Item
+    
+    var body: some View {
+        Toggle(isOn: $item.completion) {
+            Text(item.task ?? "")
+                .font(.system(.title2, design: .rounded))
+                .fontWeight(.heavy)
+                .foregroundColor(item.completion ? Color.pink : Color.primary)
+                .padding(.vertical, 8)
+                .animation(.default)
+        } //: TOGGLE
+        .onReceive(item.objectWillChange, perform: { _ in
+            if self.viewContext.hasChanges {
+                try? self.viewContext.save()
+            }
+        })
+    }
+}
